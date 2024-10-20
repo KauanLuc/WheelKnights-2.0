49be75ecede0a7a54c4d3cb86226357b3872ff8a
KauanLuc <kauan.lucena@sptech.school>
2024-10-20 02:39:37 -0300

    feat(DashboardController): Adiciona tratamento de cors ao controller

--- backend/src/main/java/wheel/knights/backend/controller/DashboardController.java
+++ backend/src/main/java/wheel/knights/backend/controller/DashboardController.java
@@ -3,6 +3,7 @@ package wheel.knights.backend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/dashboard")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class DashboardController {
     private final MiniatureService service;
 
